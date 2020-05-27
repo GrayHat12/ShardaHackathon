ae9bd6c76255f7bcb491c615e458999268bcb61b
GrayHat12 <grayhathacks10@gmail.com>
2020-05-27 16:29:09 +0530

    fixed everything

--- main2.py
+++ main2.py
@@ -11,9 +11,12 @@ options.add_argument('--disable-gpu')
 options.add_argument("--disable-logging")
 options.add_argument('log-level=3')
 
+path = r"C:\Users\Rahul\PaidProjects\ShardaHackathon\chromedriver.exe"
+
 def flights(infrom="Delhi",into="Goa",trgetmonth="february",trgetyear="2020",trgetdate="12"):
     global options
-    driver = webdriver.Chrome(executable_path=r"C:\Users\Rahul\PaidProjects\ShardaHackathon\chromedriver.exe",options=options)
+    global path
+    driver = webdriver.Chrome(executable_path=path,options=options)
     #driver = webdriver.Firefox(options=options)
     defdatedata = ["january","february","march","april","may","june","july","august","september","october","november","december"]
     driver.get('https://www.goibibo.com/flights/')
@@ -32,12 +35,12 @@ def flights(infrom="Delhi",into="Goa",trgetmonth="february",trgetyear="2020",trg
     
     drfrom = driver.find_element_by_id('gosuggest_inputSrc')
     drfrom.send_keys(infrom)
-    time.sleep(2)
+    time.sleep(3)
     driver.find_element_by_class_name('autoSuggestBoxList.flt.autoSgstFlt.autoSuggestBoxListHm').find_elements_by_tag_name('li')[0].click()
 
     drto = driver.find_element_by_id('gosuggest_inputDest')
     drto.send_keys(into)
-    time.sleep(2)
+    time.sleep(3)
     driver.find_element_by_class_name('autoSuggestBoxList.flt.autoSgstFlt.autoSuggestBoxListHm').find_elements_by_tag_name('li')[0].click()
 
     current_top_date = ["",""]
@@ -50,20 +53,32 @@ def flights(infrom="Delhi",into="Goa",trgetmonth="february",trgetyear="2020",trg
     if int(trgetyear) < int(current_top_date[1]):
         while driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')[1].lower() != trgetyear:
             #print(driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')[1])
-            driver.find_element_by_class_name('DayPicker-NavButton DayPicker-NavButton--next'.replace(' ','.')).click()
+            try:
+                driver.find_element_by_class_name('DayPicker-NavButton DayPicker-NavButton--next'.replace(' ','.')).click()
+            except:
+                time.sleep(1)
     elif int(trgetyear) > int(current_top_date[1]):
         while driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')[1].lower() != trgetyear:
             #print(driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')[1])
-            driver.find_element_by_class_name('DayPicker-NavButton DayPicker-NavButton--prev'.replace(' ','.')).click()
+            try:
+                driver.find_element_by_class_name('DayPicker-NavButton DayPicker-NavButton--prev'.replace(' ','.')).click()
+            except:
+                time.sleep(1)
 
     if defdatedata.index(current_top_date[0]) < defdatedata.index(trgetmonth):
         while driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')[0].lower() != trgetmonth:
             #print(driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')[0])
-            driver.find_element_by_class_name('DayPicker-NavButton DayPicker-NavButton--next'.replace(' ','.')).click()
+            try:
+                driver.find_element_by_class_name('DayPicker-NavButton DayPicker-NavButton--next'.replace(' ','.')).click()
+            except:
+                time.sleep(1)
     elif defdatedata.index(current_top_date[0]) > defdatedata.index(trgetmonth):
         while driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')[0].lower() != trgetmonth:
             #print(driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')[0])
-            driver.find_element_by_class_name('DayPicker-NavButton DayPicker-NavButton--prev'.replace(' ','.')).click()
+            try:
+                driver.find_element_by_class_name('DayPicker-NavButton DayPicker-NavButton--prev'.replace(' ','.')).click()
+            except:
+                time.sleep(1)
 
     #print(frmatdate)
     driver.find_element_by_id('fare_'+frmatdate).click()
@@ -72,12 +87,15 @@ def flights(infrom="Delhi",into="Goa",trgetmonth="february",trgetyear="2020",trg
     time.sleep(5)
     i=0
     buttons = driver.find_elements_by_class_name('button fr fltbook fb widthF105 fb quicks'.replace(' ','.'))
-    def_size = len('https://www.goibibo.com/flight-review/?t=f23f23a40dfe22d5fb7f782b491ff54e')
+    size = len(buttons)
+    def_size = len('https://www.goibibo.com/flight-review/?ls=1&t=ba07793b209f65b2cbbd7a43e93372f6')
     data = dict()
-    while i<len(buttons):
+    while i<size:
         but = driver.find_elements_by_class_name('button fr fltbook fb widthF105 fb quicks'.replace(' ','.'))[i]
         but.click()
+        driver.switch_to.window(driver.window_handles[1])
         curl = driver.current_url
+        time.sleep(5)
         if len(curl) == def_size:
             #print(driver.current_url)
             title = driver.find_element_by_class_name('fl mobdn ico18 padL10'.replace(' ','.')).text
@@ -103,7 +121,8 @@ def flights(infrom="Delhi",into="Goa",trgetmonth="february",trgetyear="2020",trg
                 'price' : price
             }
             data.update({str(i):tdat})
-        driver.back()
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
         i+=1
     f=open('flight.json','w+')
     f.write(str(data).replace('\'','\"'))
@@ -125,7 +144,8 @@ def suggest_places(city="New Delhi"):
 
 def hotels(rooms='1',name="Delhi",trgetmonth="february",trgetyear="2020",trgetdate="12",retmonth="february",retyear="2020",retdate="14"):
     global options
-    driver = webdriver.Chrome(executable_path=r"C:\Users\Rahul\PaidProjects\ShardaHackathon\chromedriver.exe",options=options)
+    global path
+    driver = webdriver.Chrome(executable_path=path,options=options)
     #driver = webdriver.Firefox(options=options)
     defdatedata = ["january","february","march","april","may","june","july","august","september","october","november","december"]
     driver.get('https://www.goibibo.com/hotels/')
@@ -141,39 +161,55 @@ def hotels(rooms='1',name="Delhi",trgetmonth="february",trgetyear="2020",trgetda
     if len(retmonthnum)==1:
         retmonthnum = "0"+retmonthnum
     frmatdateret = retyear+retmonthnum+retdate
-    driver.find_element_by_id('gosuggest_inputL').send_keys(name)
+    #driver.find_element_by_id('gosuggest_inputL').send_keys(name)
+    driver.find_element_by_id('downshift-1-input').send_keys(name)
     time.sleep(2)
-    driver.find_element_by_class_name('searchList').find_elements_by_tag_name('li')[0].click()
-    date_fields = driver.find_elements_by_class_name('form-control inputTxtLarge widgetCalenderTxt'.replace(' ','.'))
+    driver.find_element_by_id('downshift-1-menu').find_elements_by_tag_name('li')[0].click()
+    #driver.find_element_by_class_name('searchList').find_elements_by_tag_name('li')[0].click()
+    date_fields = driver.find_elements_by_class_name('SearchBlockUIstyles__CheckInDateWrapDiv-fity7j-13 jKVcTv'.replace(' ','.'))
     check_in = date_fields[0]
     check_out = date_fields[1]
-    if check_in.get_attribute('placeholder') == 'Choose Checkin':
-        print()
-    else:
-        check_in = check_out
-        check_out = date_fields[0]
+    #if check_in.get_attribute('placeholder') == 'Choose Checkin':
+    #    print()
+    #else:
+    #    check_in = check_out
+    #    check_out = date_fields[0]
     
     check_in.click()
+    time.sleep(1)
+    print('Check in clicked')
     current_top_date = ["",""]
-    datehead = driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')
+    datehead = driver.find_element_by_class_name('dcalendarstyles__MonthNamePara-r2jz2t-3 bVBYQn'.replace(' ','.')).text.split(' ')
     current_top_date[0]=datehead[0].lower()
     current_top_date[1]=datehead[1].lower()
     if int(trgetyear) < int(current_top_date[1]):
-        while driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')[1].lower() != trgetyear:
-            driver.find_element_by_class_name('DayPicker-NavButton DayPicker-NavButton--next'.replace(' ','.')).click()
+        while driver.find_element_by_class_name('dcalendarstyles__MonthNamePara-r2jz2t-3 bVBYQn'.replace(' ','.')).text.split(' ')[1].lower() != trgetyear:
+            try:
+                driver.find_element_by_class_name('dcalendarstyles__MonthChangeRightArrowDiv-r2jz2t-16 eVCvYn'.replace(' ','.')).click()
+            except:
+                time.sleep(1)
     elif int(trgetyear) > int(current_top_date[1]):
-        while driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')[1].lower() != trgetmonth:
-            driver.find_element_by_class_name('DayPicker-NavButton DayPicker-NavButton--prev'.replace(' ','.')).click()
+        while driver.find_element_by_class_name('dcalendarstyles__MonthNamePara-r2jz2t-3 bVBYQn'.replace(' ','.')).text.split(' ')[1].lower() != trgetmonth:
+            try:
+                driver.find_element_by_class_name('dcalendarstyles__MonthChangeLeftArrowDiv-r2jz2t-15 jxjYIU'.replace(' ','.')).click()
+            except:
+                time.sleep(1)
     
     if defdatedata.index(current_top_date[0]) < defdatedata.index(trgetmonth):
-        while driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')[0].lower() != trgetmonth:
-            driver.find_element_by_class_name('DayPicker-NavButton DayPicker-NavButton--next'.replace(' ','.')).click()
+        while driver.find_element_by_class_name('dcalendarstyles__MonthNamePara-r2jz2t-3 bVBYQn'.replace(' ','.')).text.split(' ')[0].lower() != trgetmonth:
+            try:
+                driver.find_element_by_class_name('dcalendarstyles__MonthChangeRightArrowDiv-r2jz2t-16 eVCvYn'.replace(' ','.')).click()
+            except:
+                time.sleep(1)
     elif defdatedata.index(current_top_date[0]) > defdatedata.index(trgetmonth):
-        while driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')[0].lower() != trgetmonth:
-            driver.find_element_by_class_name('DayPicker-NavButton DayPicker-NavButton--prev'.replace(' ','.')).click()
+        while driver.find_element_by_class_name('dcalendarstyles__MonthNamePara-r2jz2t-3 bVBYQn'.replace(' ','.')).text.split(' ')[0].lower() != trgetmonth:
+            try:
+                driver.find_element_by_class_name('dcalendarstyles__MonthChangeLeftArrowDiv-r2jz2t-15 jxjYIU'.replace(' ','.')).click()
+            except:
+                time.sleep(1)
 
     #driver.find_element_by_id('fare_'+frmatdate).click()
-    for el in driver.find_elements_by_class_name('DayPicker-Day'):
+    for el in driver.find_elements_by_class_name('dcalendarstyles__DateWrapDiv-r2jz2t-7 SVVbO'.replace(' ','.')):
         try:
             if int(el.text) == int(trgetdate):
                 el.click()
@@ -181,27 +217,41 @@ def hotels(rooms='1',name="Delhi",trgetmonth="february",trgetyear="2020",trgetda
         except:
             pass
     
+    print('check in done')
     time.sleep(2)
-    #check_out.click()
-    datehead = driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')
+    check_out.click()
+    time.sleep(2)
+    datehead = driver.find_element_by_class_name('dcalendarstyles__MonthNamePara-r2jz2t-3 bVBYQn').text.split(' ')
     current_top_date[0]=datehead[0].lower()
     current_top_date[1]=datehead[1].lower()
     if int(retyear) < int(current_top_date[1]):
-        while driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')[1].lower() != retyear:
-            driver.find_element_by_class_name('DayPicker-NavButton DayPicker-NavButton--next'.replace(' ','.')).click()
+        while driver.find_element_by_class_name('dcalendarstyles__MonthNamePara-r2jz2t-3 bVBYQn').text.split(' ')[1].lower() != retyear:
+            try:
+                driver.find_element_by_class_name('dcalendarstyles__MonthChangeRightArrowDiv-r2jz2t-16 eVCvYn'.replace(' ','.')).click()
+            except:
+                time.sleep(1)
     elif int(retyear) > int(current_top_date[1]):
-        while driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')[1].lower() != retyear:
-            driver.find_element_by_class_name('DayPicker-NavButton DayPicker-NavButton--prev'.replace(' ','.')).click()
+        while driver.find_element_by_class_name('dcalendarstyles__MonthNamePara-r2jz2t-3 bVBYQn').text.split(' ')[1].lower() != retyear:
+            try:
+                driver.find_element_by_class_name('dcalendarstyles__MonthChangeLeftArrowDiv-r2jz2t-15 jxjYIU'.replace(' ','.')).click()
+            except:
+                time.sleep(1)
     
     if defdatedata.index(current_top_date[0]) < defdatedata.index(retmonth):
-        while driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')[0].lower() != retmonth:
-            driver.find_element_by_class_name('DayPicker-NavButton DayPicker-NavButton--next'.replace(' ','.')).click()
+        while driver.find_element_by_class_name('dcalendarstyles__MonthNamePara-r2jz2t-3 bVBYQn').text.split(' ')[0].lower() != retmonth:
+            try:
+                driver.find_element_by_class_name('dcalendarstyles__MonthChangeRightArrowDiv-r2jz2t-16 eVCvYn'.replace(' ','.')).click()
+            except:
+                time.sleep(1)
     elif defdatedata.index(current_top_date[0]) > defdatedata.index(retmonth):
-        while driver.find_element_by_class_name('DayPicker-Caption').text.split(' ')[0].lower() != retmonth:
-            driver.find_element_by_class_name('DayPicker-NavButton DayPicker-NavButton--prev'.replace(' ','.')).click()
+        while driver.find_element_by_class_name('dcalendarstyles__MonthNamePara-r2jz2t-3 bVBYQn').text.split(' ')[0].lower() != retmonth:
+            try:
+                driver.find_element_by_class_name('dcalendarstyles__MonthChangeLeftArrowDiv-r2jz2t-15 jxjYIU'.replace(' ','.')).click()
+            except:
+                time.sleep(1)
 
     #driver.find_element_by_id('fare_'+frmatdateret).click()
-    for el in driver.find_elements_by_class_name('DayPicker-Day'):
+    for el in driver.find_elements_by_class_name('dcalendarstyles__DateWrapDiv-r2jz2t-7 SVVbO'.replace(' ','.')):
         try:
             if int(el.text) == int(retdate):
                 el.click()
@@ -275,3 +325,67 @@ def getPlaces(snt=[]):
             places.append(ch0+' '+ch1)
         i+=1
     return places
+           
+def customHotels(rooms=1,guest=1,name="New Delhi",trgetmonth='02',trgetyear='2020',trgetdate='12',retmonth='02',retyear='2020',retdate='14'):
+    global options
+    global path
+    data = json.load(open('cities.json','r+',encoding='utf-8'))
+    fname = name.replace(' ','').lower()
+    cities = list(data.keys())
+    if fname not in cities:
+        print('City Not Found')
+        return -1
+    driver = webdriver.Chrome(options=options,executable_path=path)
+    driver.get('https://www.goibibo.com/hotels/')
+    cookies = driver.get_cookies()
+    fcookie = ''
+    names = []
+    for cook in cookies:
+        if cook['name'] in names:
+            continue
+        names.append(cook['name'])
+        fcookie+= cook['name']+'='+cook['value']+';'
+    #print(fcookie)
+    driver.close()
+    url = 'https://hermes.goibibo.com/hotels/v12/search/data/v3/cityid/fromdate/todate/rooms-guest-0?s=popularity&cur=INR&tmz=-330'
+    url = url.replace('cityid',data[fname]['id'])
+    url = url.replace('fromdate',trgetyear+trgetmonth+trgetdate)
+    url = url.replace('todate',retyear+retmonth+retdate)
+    url = url.replace('rooms',str(rooms))
+    url = url.replace('guest',str(guest))
+    print(url)
+    headers = {
+        "User-Agent": "PostmanRuntime/7.25.0",
+        'authority' : 'www.goibibo.com',
+        'cookie' : fcookie,
+        'host' : 'hermes.goibibo.com',
+        'accept' : '*/*',
+        'Accept-Encoding' : 'gzip, deflate, br',
+        'Connection' : 'keep-alive'
+        }
+    resp = requests.get(url,headers=headers)
+    #print(resp.headers)
+    #print(364,resp.request.headers)
+    hotelData = resp.text
+    #print(hotelData)
+    hotelData = json.loads(hotelData)
+    json.dump(hotelData,open('hotel.json','w+',encoding='utf-8'))
+    return hotelData
+
+"""def loadCities(path='placeid.csv'):
+    data = {}
+    text = open(path,'r',encoding='utf-8').read()
+    lines = text.split('\n')
+    lines.pop(0)
+    for line in lines:
+        line = line.replace(' ','')
+        line = line.lower()
+        dats = line.split(',')
+        name = dats[0][1:-1]
+        id = dats[1][1:-1]
+        domestic = dats[2][1:-1]
+        data[name] = {'id' : id, 'domestic': domestic}
+    with open('cities.json','w+',encoding='utf-8') as f:
+        json.dump(data,f)
+
+loadCities()"""
--- start.py
+++ start.py
@@ -3,23 +3,26 @@ import requests
 import keyboard
 import main2
 from os import system
+import json
+
+# https://www.goibibo.com/hotels/the-daanish-residency-hotel-in-Delhi-1436991033012202026/?hquery={%22ci%22:%2220200606%22,%22co%22:%2220200608%22,%22r%22:%221-2-0%22,%22ibp%22:%22v3%22}&hmd=07e8570856fee84deaed9e8c2cdc2f2d88ac7d198e7086f95d4d92da417db27e157e849661f40a56468c45567f3c07079d7c313032b7267bb0713951e009e35fbd8c429870d1aa3344b364fbde63111f112d3ad244d788701917f43b386f44cec0791fb3b334d469e1ddd13ae081f2726f210a8233c6731124a5a1fb55abc048a5d3293680d782250f3b181333b31bbdfe8b5fb9d4e1c32e414a0d1bb7bf347a735db9894b75760a2582c3875cc0c3a6148c4345ab66017499b084706a17c4a02e590eb2ad51b540d60539a9945b2f28a2c03c218286f883056fd83bdadbe2ea92f26983717d01d527c4911e79f82d890aab59f98b6722d9fa18b45d35bd8d108b8821157e63b904f98b29b6a2df39c8288a54ecca08ff903bc8a1e25ac3&cc=IN
 
 def show_menu():
     global selected
     str0="Choose an option:"+'\n'
     str0+="{1} {0}. Hotel Booking {0} {2}".format(1, ">" if selected == 1 else " ", "<" if selected == 1 else " ")+'\n'
     str0+="{1} {0}. Flight Booking {0} {2}".format(2, ">" if selected == 2 else " ", "<" if selected == 2 else " ")+'\n'
-    str0+="{1} {0}. Suggestions {0} {2}".format(3, ">" if selected == 3 else " ", "<" if selected == 3 else " ")
+    str0+="{1} {0}. Suggestions {0} {2}".format(3, ">" if selected == 3 else " ", "<" if selected == 3 else " ")+'\n'
+    str0+="{1} {0}. Exit {0} {2}".format(4, ">" if selected == 4 else " ", "<" if selected == 4 else " ")
     print(str0)
 
 def hotel_book():
     print('\n\n')
     hot = pyfiglet.figlet_format("Hotel Service",font='computer',width=100)
-    print(hot)
+    #print(hot)
     print('\n\n')
     input()
     area = input('Area Name / Hotel Name : ')
-    rooms = str(int(input('Number of rooms : ')))
     date = input('Date Checkin (dd/mm/yyyy) : ')
     datedata = []
     defdatedata = ["january","february","march","april","may","june","july","august","september","october","november","december"]
@@ -28,7 +31,6 @@ def hotel_book():
         datedata.append(date[0])
         datedata.append(date[1])
         datedata.append(date[2])
-        datedata[1] = defdatedata[int(datedata[1])+1]
     else:
         print('Incorrect Date format ',date)
         return
@@ -39,20 +41,35 @@ def hotel_book():
         datedataout.append(date[0])
         datedataout.append(date[1])
         datedataout.append(date[2])
-        datedataout[1] = defdatedata[int(datedataout[1])+1]
     else:
         print('Incorrect Date format ',date)
         return
-    data = main2.hotels(rooms,area,datedata[1],datedata[2],datedata[0],datedataout[1],datedataout[2],datedataout[0])
-    print('\n\nHotels Found :',len(data.keys()),'\n\n')
-    for key in data.keys():
-        print('\n\n')
-        print("URL :",data.get(key)['url'])
-        print("IMAGE :",data.get(key)['img'])
-        print("NAME :",data.get(key)['name'])
-        print("AREA :",data.get(key)['area'])
-        #print(data.get(key)['status'])
-        print("PRICE :",data.get(key)['price'])
+    rooms = int(input('Rooms : '))
+    guests = int(input('Guests : '))
+    resp = main2.customHotels(rooms,guests,area,datedata[1],datedata[2],datedata[0],datedataout[1],datedataout[2],datedataout[0])
+    if resp == -1:
+        print('You Entered an Invalid City Name')
+        return
+    data = []
+    for dat in resp['data']:
+        cdat = {
+            'name' : dat['hn'],
+            'area' : dat['l'],
+            'image' : dat['t'],
+            'desc' : dat['rtn'],
+            'id' : dat['hc'],
+            'price' : dat['spr'],
+            'url name' : dat['hn'].replace(' ','-')
+        }
+        data.append(cdat)
+    for dat in data:
+        print('----'*40)
+        print('Name :',dat['name'])
+        print('Area :',dat['area'])
+        print('Image :',dat['image'])
+        print('Description :',dat['desc'])
+        print('Url :','https://www.goibibo.com/hotels/name-hid/'.replace('hid',dat['id']).replace('name',dat['url name']))
+        print('Price :',dat['price'])
     
 def flight_book():
     print('\n\n')
@@ -70,11 +87,16 @@ def flight_book():
         datedata.append(date[0])
         datedata.append(date[1])
         datedata.append(date[2])
-        datedata[1] = defdatedata[int(datedata[1])+1]
+        datedata[1] = defdatedata[int(datedata[1])-1]
     else:
         print('Incorrect Date format ',date)
         return
-    data = main2.flights(area,rooms,datedata[1],datedata[2],datedata[0])
+    try:
+        data = main2.flights(area,rooms,datedata[1],datedata[2],datedata[0])
+    except Exception as ex:
+        print('Some unexpected Error Occured')
+        print(ex)
+        return
     print('\n\nFlights Found :',len(data.keys()),'\n\n')
     for key in data.keys():
         print('\n\n')
@@ -93,7 +115,12 @@ def suggest():
     print('\n\n')
     input()
     area=input('City : ')
-    data = main2.suggest_places(area)
+    try:
+        data = main2.suggest_places(area)
+    except Exception as ex:
+        print('Some unexpected Error Occured')
+        print(ex)
+        return
     for res in data['results']:
         print()
         print(res['formatted_address'])
@@ -109,7 +136,7 @@ def up():
 
 def down():
     global selected
-    if selected == 3:
+    if selected == 4:
         return
     _=system('cls')
     selected += 1
@@ -126,14 +153,29 @@ def enter():
         flight_book()
     elif selected == 3:
         suggest()
+    elif selected == 4:
+        _=system('cls')
+        print('DONE')
+        exit(0)
+    print('\n(backspace to return to menu)')
+
+def backspace():
+    _=system('cls')
+    print(pig)
+    show_menu()
+
 
 _=system('cls')
-pig = pyfiglet.figlet_format("GrayBot",font='computer',width=200)
+pig = pyfiglet.figlet_format("PBL Project",font='computer',width=200)
 print(pig)
 
 selected = 1
-show_menu()
+try:
+    show_menu()
+except:
+    keyboard.unhook_all()
 keyboard.add_hotkey('up', up)
 keyboard.add_hotkey('down', down)
 keyboard.add_hotkey('enter',enter)
+keyboard.add_hotkey('backspace',backspace)
 keyboard.wait()
